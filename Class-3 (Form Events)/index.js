6b625dd903739b1df987abe3b98296a3ee288fab
mrinal1224 <iammrinalkc@gmail.com>
2024-11-20 10:38:46 +0530

    push

--- Class-3 (Form Events)/index.js
+++ Class-3 (Form Events)/index.js
@@ -1,14 +1,38 @@
 const myForm = document.querySelector('.feedback-form')
 console.log(myForm)
+const p = document.querySelector('p')
+
+let usernameRegex = /^[a-z0-9]{3,12}$/
+let userFeedbackRegex = /^.{20,50}$/
 
 myForm.addEventListener('submit' , function(e){
       e.preventDefault()
       let usernameValue = myForm.username.value
       let feedbackValue = myForm.feedback.value
 
-      console.log(usernameValue)
-      console.log(feedbackValue)
+      let userNameValidation = usernameRegex.test(usernameValue)
+      let feedbackValidation = userFeedbackRegex.test(feedbackValue)
+
+     console.log(userNameValidation)
+     console.log(feedbackValidation)
+
+
+     if(userNameValidation && feedbackValidation){
+        p.innerText='All validated Successfully'
+     }
+
+     else{
+        p.innerText='Validation failed check Again'
+     }
 })
 
 
-// let exp = //
+// const str = 'test123AB'
+
+// let result = usernameRegex.test(str)
+
+
+// console.log(result)
+
+
+
