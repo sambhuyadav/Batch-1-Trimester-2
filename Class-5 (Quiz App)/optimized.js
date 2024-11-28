cbc4df44ce705aea41f964b324396edd81c4c57e
mrinal1224 <iammrinalkc@gmail.com>
2024-11-28 11:39:58 +0530

    push

---
+++ Class-5 (Quiz App)/optimized.js
@@ -0,0 +1,22 @@
+const correctAns = ["D", "B", "C", "B", "D"];
+let questions = document.querySelectorAll(".question");
+const result = document.querySelector(".result");
+
+const form = document.querySelector(".quiz-form");
+form.addEventListener("submit", function (e) {
+  e.preventDefault();
+
+  let score = 0;
+  correctAns.forEach((answer, index) => {
+    const userAnswer = form[`q${index + 1}`].value; // Access input value directly
+    if (userAnswer === answer) {
+      score += 1;
+      questions[index].classList.add("correct");
+    } else {
+      questions[index].classList.add("wrong");
+    }
+  });
+
+  result.classList.remove("hide");
+  result.querySelector("p").innerText = `You Scored ${score}/5`;
+});
